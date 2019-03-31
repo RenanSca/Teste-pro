bfb414f0a25337029416adeae237c6191d1456b9
RenanSca <45311457+RenanSca@users.noreply.github.com>
2019-03-30 23:06:59 -0300

    Segundo commit

--- index.js
+++ index.js
@@ -33,3 +33,16 @@
 			    }
 			  }
 			});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+  $(document).ready(function() {
+    Materialize.updateTextFields();
+  });
